3a9503466e53a68edcde4348a35ac11afa66be17
Tal <talbsho@gmail.com>
2023-01-14 09:09:28 -0500

    export guide

--- src/components/common/index.ts
+++ src/components/common/index.ts
@@ -1,5 +1,6 @@
 import Image from './image';
 import Toggle from './toggle';
+import { UserGuide } from './UserGuide';
 import * as Buttons from './buttons';
 
-export { Image, Toggle, Buttons };
+export { Image, Toggle, Buttons, UserGuide };
