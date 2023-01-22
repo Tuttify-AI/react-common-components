9cdbc80ded8dff723b94afa4dc842e273a70810d
Tal <talbsho@gmail.com>
2023-01-22 08:23:40 -0500

    fix arcade

--- src/components/common/Arcade/Arcade.tsx
+++ src/components/common/Arcade/Arcade.tsx
@@ -1,17 +1,44 @@
 import React from 'react';
-import { Modal } from '../Modal';
 
 import './index.scss';
 
 export interface Props {
   url: string;
   open: boolean;
+  onClose: () => void;
 }
 
-export const Arcade = ({ url, open }: Props) => {
+export const Arcade = ({ url, open, onClose }: Props) => {
+  if (!open) {
+    return null;
+  }
+
   return (
-    <Modal open={open}>
-      <iframe className="arcadeFrame" src={url} />
-    </Modal>
+    <div
+      style={{
+        position: 'fixed',
+        background: 'rgba(0,0,0,.5)',
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        top: 0,
+        left: 0,
+        width: '100%',
+        height: '100%',
+        zIndex: 1001,
+      }}
+      onClick={() => onClose()}
+    >
+      <div
+        style={{
+          width: '50%',
+          height: 'calc(61.416666666666664% + 41px)',
+          position: 'relative',
+        }}
+        onClick={() => onClose()}
+      >
+        <iframe className="arcadeFrame" src={url} />
+      </div>
+    </div>
   );
 };
